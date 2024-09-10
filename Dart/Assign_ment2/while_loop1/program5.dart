b0a08af03d3a404a7f4eaf3cfb1ba85f02d1417d
akash7577 <akashshinde7577@gmail.com>
2024-09-10 22:09:01 +0530

    codes

---
+++ Dart/Assign_ment2/while_loop1/program5.dart
@@ -0,0 +1,11 @@
+void main(){
+  int start = 50;
+  int end = 30;
+  while(start>=end){
+    if(start%2==0){
+      print(start*start);
+      
+    }
+   start--;
+  }
+}
---
+++ flutter_application_1/lib/datatype.dart
@@ -0,0 +1,15 @@
+//data type
+void main(){
+     // 1/intterger
+     int x = 10;
+     //2 double
+     double y = 12.4;
+     //3 string 
+     String str = "akash";
+     //4 
+     
+     print(x);
+     print(y);
+     print(str);
+
+}
